f80a1777cf3116a3ca5b6411e10a19f65cf77521
Nyan Lin Htut <nyanlinhtut925@gmail.com>
2025-09-09 16:00:22 +0700

    temporialy comment out scheduler

--- config/initializers/after_initialize_scheduler.rb
+++ config/initializers/after_initialize_scheduler.rb
@@ -1,23 +1,23 @@
-Rails.application.config.after_initialize do
-  if defined?(Sidekiq) && Sidekiq.respond_to?(:configure_server)
-    # Add BlueskyBridge scheduler to existing Sidekiq configuration
-    bluesky_scheduler_config = {
-      'follow_bluesky_bot_scheduler' => {
-        'every' => '10m',
-        'class' => 'Scheduler::FollowBlueskyBotScheduler',
-        'queue' => 'scheduler'
-      }
-    }
+# Rails.application.config.after_initialize do
+#   if defined?(Sidekiq) && Sidekiq.respond_to?(:configure_server)
+#     # Add BlueskyBridge scheduler to existing Sidekiq configuration
+#     bluesky_scheduler_config = {
+#       'follow_bluesky_bot_scheduler' => {
+#         'every' => '10m',
+#         'class' => 'Scheduler::FollowBlueskyBotScheduler',
+#         'queue' => 'scheduler'
+#       }
+#     }
     
-    # Use the new Sidekiq configuration API
-    Sidekiq.configure_server do |config|
-      config[:scheduler] ||= {}
-      config[:scheduler][:schedule] ||= {}
+#     # Use the new Sidekiq configuration API
+#     Sidekiq.configure_server do |config|
+#       config[:scheduler] ||= {}
+#       config[:scheduler][:schedule] ||= {}
       
-      # Merge our scheduler into the existing schedule
-      config[:scheduler][:schedule].merge!(bluesky_scheduler_config)
-    end
-  else
-    Rails.logger.warn "BlueskyBridge: Sidekiq not available, scheduler not configured"
-  end
-end
+#       # Merge our scheduler into the existing schedule
+#       config[:scheduler][:schedule].merge!(bluesky_scheduler_config)
+#     end
+#   else
+#     Rails.logger.warn "BlueskyBridge: Sidekiq not available, scheduler not configured"
+#   end
+# end
